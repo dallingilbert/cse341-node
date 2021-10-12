7363a4e40e8375cea728d44649b31943009da909
Dallin Gilbert <djgilb24@byui.edu>
2021-10-11 22:19:14 -0600

    adding functionality for team activity 2

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,61 @@
 const express = require('express');
 const router = express.Router();
 
+const userNames = ['Dallin', 'Joe', 'Gage'];
+let isOldUser = false;
+let canAdd = false;
+
+router.post('/addUser', (req, res, next) => {
+  const newUser = req.body.username;
+  
+  canAdd = userNames.includes(newUser);
+
+  if (!canAdd)
+    userNames.push(newUser);
+    
+  res.redirect('/ta02');
+});
+
+router.post('/removeUser', (req, res, next) => {
+  const oldUser = req.body.remove;
+
+  let isSameUser = (user) => {
+    if (user === oldUser){
+      return true;
+    }
+    else {
+      return false;
+    }
+  };
+  
+  // get user index
+  let index = userNames.findIndex(isSameUser);
+
+  // check if user is in our array
+  if (index !== -1) {
+    userNames.splice(index, 1);
+    isOldUser = false;
+  }
+  else 
+    isOldUser = true;
+
+  console.log(index);
+
+  res.redirect('/ta02');
+});
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
-  });
+    user: userNames,
+    oldUser: isOldUser,
+    isAddable: canAdd
+  }); 
 });
+
+
 
 module.exports = router;
