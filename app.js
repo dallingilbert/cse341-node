fedb9d1be556011ed899342d80f85a904cb799c2
Dallin Gilbert <djgilb24@byui.edu>
2021-10-18 17:09:53 -0600

    adding MongoDB functionality to shop

--- app.js
+++ app.js
@@ -21,11 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('616b6d59314620843e232a43')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
-  next();
 });
 
 app.use('/admin', adminRoutes);
