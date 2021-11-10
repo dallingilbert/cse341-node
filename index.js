8230f84daacd03a4fbb660e5e06cef3722857a0a
Dallin Gilbert <djgilb24@byui.edu>
2021-11-10 15:15:05 -0700

    setting headers for api fetch

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 // Our initial setup (package requires, port number setup)
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
-const routes = require('./routes');
+const routes = require("./routes");
 
 const app = express();
 
 app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .use(bodyParser({ extended: false })) // For parsing the body of a POST
-  .use('/', routes)
-  .get('/', routes)
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST",
+      "PUT",
+      "PATCH",
+      "DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    next();
+  })
+  .use(bodyParser.json()) // application/json
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  //.use(bodyParser({ extended: false })) // For parsing the body of a POST
+  .use("/", routes)
+  .get("/", routes)
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
